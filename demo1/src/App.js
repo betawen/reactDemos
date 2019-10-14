4e3aebdcda851d2816daf1d1460232185ac86c21
betawen <jing_find@163.com>
2019-10-14 11:43:15 +0800

    init demo1 with react

---
+++ demo1/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div id="qiqi">没懂为什么一开始居然是只读权限，气气</div>
+      <div className="qiqi">样式关键字居然是className？</div>
+    </div>
+  );
+}
+
+export default App;
